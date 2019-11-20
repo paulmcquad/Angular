cfa0cf1a619a2737978c9e647fb5731cd7c48778
Paul McQuade <paulmcquad@gmail.com>
2019-11-20 22:36:37 +0000

    Added hello-world project
    remove old project
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- hello-world/src/app/app.component.spec.ts
+++ hello-world/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first-app'`, () => {
+  it(`should have as title 'hello-world'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('first-app');
+    expect(app.title).toEqual('hello-world');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to first-app!');
+    expect(compiled.querySelector('.content span').textContent).toContain('hello-world app is running!');
   });
 });
